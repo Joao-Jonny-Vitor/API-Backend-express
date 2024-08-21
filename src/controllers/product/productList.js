6351f0a5ce699c136a7df266ba92bb4c4579acdb
João Vitor (Jonny) <joao.jonny.vitor@gmail.com>
2024-08-21 20:37:29 -0300

    add env vars

--- src/controllers/product/productList.js
+++ src/controllers/product/productList.js
@@ -1,8 +1,9 @@
-import productModel from "../../models/productModel.js"
+//import productModel from "../../models/productModel.js"
 
 const productList = async(req, res) => {
 
-    const product = await productModel.getAll()
+    //const product = await productModel.getAll()
+    const product = []
     res.json(product)
 }
 
--- src/controllers/user/userList.js
+++ src/controllers/user/userList.js
@@ -1,8 +1,9 @@
-import userModel from "../../models/userModel.js"
+//import userModel from "../../models/userModel.js"
 
 const userList = async(req, res) => {
 
-    const users = await userModel.getAll()
+    //const users = await userModel.getAll()
+    const users = []
     res.json(users)
 }
 
